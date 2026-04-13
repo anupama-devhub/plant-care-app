ae47c34dbebc2bbc92ceddbf44ac7b8d0c562ea9
Anupama Singh <anupama.singh05@sap.com>
2026-04-13 10:28:50 +0530

    add folder structure

---
+++ plant-service/src/test/java/com/plantcare/plant/PlantServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plantcare.plant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlantServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
